c70ef600ab0778d0ad57d3900977753ad96214b4
Maxwell Da Silva <dayvson@gmail.com>
2015-02-17 12:38:52 -0500

    Basic gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,75 @@
+/*
+ * grunt-air-sdk
+ * https://github.com/dayvson/grunt-air-sdk
+ *
+ * Copyright (c) 2015 Maxwell Da Silva
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+
+      ],
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    // // Before generating any new files, remove any previously-created files.
+    // clean: {
+    //   tests: ['tmp']
+    // },
+
+    // Configuration to be run (and then tested).
+    air_sdk: {
+      hello_world: {
+        options: {
+          bin:"mxmlc",
+          rawConfig: "+configname=air --optimize"
+        },
+        files: {
+          'test/expected/HelloWorldApp2.swf': 'test/fixtures/HelloWorldApp.as'
+        }
+      },
+      hello_world_swc: {
+        options: {
+          bin:"compc",
+          rawConfig: "-static-link-runtime-shared-libraries=true -target-player=10.1 -source-path test/fixtures -include-classes com.dayvson.lib.Sample"
+        },
+        files: {
+          'test/expected/HelloWorldApp.swc': 'test/fixtures/HelloWorldApp.as'
+        }
+      },
+    },
+
+    // // Unit tests.
+    // nodeunit: {
+    //   tests: ['test/*_test.js']
+    // }
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  //grunt.registerTask('test', ['clean', 'air_sdk', 'nodeunit']);
+grunt.registerTask('test', ['air_sdk']);
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
